0033801cc7c3f1ca32a5df38354645b8ec1ed242
Marc Lepage <mlepage@antimeta.com>
2014-08-21 14:04:35 -0400

    Add score

--- Assets/Scripts/DestroyMonsterOnCollision.cs
+++ Assets/Scripts/DestroyMonsterOnCollision.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class DestroyMonsterOnCollision : MonoBehaviour {
 
+	// The on screen label for the score
+	public GUIText scoreLabel;
+
+	// The number of monsters destroyed
+	private int score = 0;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -16,7 +22,14 @@ public class DestroyMonsterOnCollision : MonoBehaviour {
 	// Called when the controller hits a collider while performing a Move
 	void OnControllerColliderHit (ControllerColliderHit hit) {
 		if (hit.gameObject.tag == "Monster") {
+			// Destroy any monsters we hit
 			Destroy (hit.gameObject);
+
+			// Also increase score
+			++score;
+
+			// And update the score's GUI text
+			scoreLabel.text = "Score: " + score;
 		}
 	}
 
