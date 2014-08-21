a42d7b88eb9728dd622232310e9d1ff297b3bbe8
Marc Lepage <mlepage@antimeta.com>
2014-08-21 13:24:05 -0400

    Add player

---
+++ Assets/Scripts/DestroyMonsterOnCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyMonsterOnCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	// Called when the controller hits a collider while performing a Move
+	void OnControllerColliderHit (ControllerColliderHit hit) {
+		if (hit.gameObject.tag == "Monster") {
+			Destroy (hit.gameObject);
+		}
+	}
+
+}
